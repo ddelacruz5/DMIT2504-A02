11f136c4503cab9a0ca1c55ad80c6b3353f3db31
ddelacruz5 <ddelacruz5@nait.ca>
2026-09-15 10:31:13 -0600

    JSON parsing and formatting, which is mid at best

--- 01 - dart + flutter intro/ex04 - async + web data exercise/student/dictionary-api/bin/dictionary_api.dart
+++ 01 - dart + flutter intro/ex04 - async + web data exercise/student/dictionary-api/bin/dictionary_api.dart
@@ -24,12 +24,15 @@
 
 // END program
 
+// IMPORTS
 import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-Future<void> main() async {
-  while (true) {
+Future<void> main() async { 
+  // "Future" is a promise saying 'i don't have anything yet, but i will later.'
+  // To 'unwrap' a Future and get a real value when it's ready, you mark the fuction as asyc
+  while (true) { // Looping until the user quits
     print('Enter a word (or quit to exit):');
     String? input = stdin.readLineSync();
 
@@ -38,12 +41,18 @@ Future<void> main() async {
       break;
     }
 
+    //http packge turns webaddresses into Uri objects.
+    // they can be built using Uri.Parse(), which takes a string and turns it into a proper URL
     String url = 'https://api.dictionaryapi.dev/api/v2/entries/en/$input';
     Uri myUri = Uri.parse(url);
 
     var response = await http.get(myUri);
+    // after async, use the 'await' keyword when calling.
+    // 'await' is saying 'pause, don't run the next line until this finishes.'
 
     if (response.statusCode == 200) {
+      // JSON parsing hell. Maybe ask Oliver about this
+      // The API returns a list of entires, each entry is a map(key-value pairs)
       var data = jsonDecode(response.body);
       var firstEntry = data[0];
       var meanings = firstEntry['meanings'];
@@ -51,7 +60,11 @@ Future<void> main() async {
       var definitions = firstMeaning['definitions']; // list of definition objects
       var firstDefinitionObject = definitions[0]; // the first one (still a map)
       var definitionText = firstDefinitionObject['definition']; // now it's the plain text
-      print('$definitionText');
+      
+      // makin it look pretty
+      print('Word'.padRight(15) + 'Definition');
+      print('-' * 50);
+      print('$input'.padRight(15) + '$definitionText');
     } else {
       print('Something went wrong. Status: ${response.statusCode}');
     }
