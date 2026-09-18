93d9dd53aadb5fa4130b7ffcac8054763381e31a
ddelacruz5 <ddelacruz5@nait.ca>
2026-09-18 14:49:31 -0600

    ex02 dart practice menu logic

--- 01 - dart + flutter intro/ex02 - dart language exercises/student/dart-exercises/bin/dart_exercises.dart
+++ 01 - dart + flutter intro/ex02 - dart language exercises/student/dart-exercises/bin/dart_exercises.dart
@@ -1,12 +1,13 @@
 import 'package:dart_exercises/dart_exercises.dart' as dart_exercises;
 
+import 'dart:io';
+
 class Student {
   String firstName = '';
   String lastName = '';
   int idNumber = 0;
 
-  Student.empty() {
-  }
+  Student.empty() {}
 
   Student(String firstName, String lastName, int idNumber) {
     this.firstName = firstName;
@@ -42,12 +43,41 @@ class Student {
   String getFullName() {
     return '$lastName, $firstName';
   }
-
 }
 
 void main(List<String> arguments) {
-  Student jane = Student('Jane', 'Doe', 101);
-  print(jane.getFullName());
-  jane.setFirstName('JaneNewName');
-  print(jane.getFullName());
+  List<Student> roster = [];
+
+  bool running = true;
+  while (running) {
+    print('1. Do thing A');
+    print('2. Do thing B');
+    print('3. Quit');
+
+    String? input = stdin.readLineSync();
+
+    if (input != null) {
+      int? choice = int.tryParse(input);
+      if (choice != null) {
+        switch (choice) {
+          case 1:
+            print('You picked one');
+            break;
+          case 2:
+            print('You picked two');
+            break;
+          case 3:
+            print('Exiting...');
+            running = false;
+            break;
+          default:
+            print('Unrecognized Input.');
+        }
+      } else {
+        print('Please enter a valid option');
+      }
+    } else {
+      print('Please enter a valid option');
+    }
+  }
 }
