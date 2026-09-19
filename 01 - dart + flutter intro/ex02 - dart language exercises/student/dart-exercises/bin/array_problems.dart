dc07d03f22da9a6ce7a0243d82f14fe1385b459d
ddelacruz5 <ddelacruz5@nait.ca>
2026-09-19 17:51:18 -0600

    01/02/array_problems created .dart and program now accepts input

---
+++ 01 - dart + flutter intro/ex02 - dart language exercises/student/dart-exercises/bin/array_problems.dart
@@ -0,0 +1,23 @@
+// Get input from the user: How many values to pick, and the range
+// Create storage for the picked values, sized appropriately
+// Generate random numbers within range
+// Check for duplicates before accepting a number into the pick
+// Display the result
+
+import 'dart:io';
+
+void main(List<String> arguments) {
+int? intValue;
+int? intRange;
+
+  // Get input from user
+  print('Enter the number of values');
+  String? inputValue = stdin.readLineSync();
+  print('Enter the range');
+  String? inputRange = stdin.readLineSync();
+  // Check input is not null
+  if (inputValue != null && inputRange != null) {
+    intValue = int.tryParse(inputValue);
+    intRange = int.tryParse(inputRange);
+  }
+}
